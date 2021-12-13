c2fc3261907b1c15611844f5060ac3a3fa6de2d5
Shawn Chen <sschenshawn@gmail.com>
2021-12-12 21:40:18 -0500

    Define a helper function to calculate cosine similarity

--- wcmp_alloc/group_reduction.py
+++ wcmp_alloc/group_reduction.py
@@ -1,6 +1,6 @@
 import gurobipy as gp
 from gurobipy import GRB
-from math import gcd
+from math import gcd, sqrt
 from functools import reduce
 
 # If True, feeds solver with scaled up integer groups.
@@ -24,6 +24,20 @@ def frac2int_lossless(frac_list):
     while any(list(map(lambda x: x % 1, frac_list))):
         frac_list = list(map(lambda x: x * 10, frac_list))
     return gcd_reduce(list(map(int, frac_list)))
+
+def cosine_similarity(G1, G2):
+    '''
+    Computes the cosine similarity between group G1 and G2. They must be of the
+    same size.
+    '''
+    assert len(G1) == len(G2) and G1 and G2, 'G1 and G2 must be non-empty ' \
+                                             'equal size.'
+    dot_sum, g1_l2_norm_sq, g2_l2_norm_sq = 0, 0, 0
+    for i in range(len(G1)):
+        dot_sum += G1[i] * G2[i]
+        g1_l2_norm_sq += G1[i] * G1[i]
+        g2_l2_norm_sq += G2[i] * G2[i]
+    return dot_sum / (sqrt(g1_l2_norm_sq) * sqrt(g2_l2_norm_sq))
 
 class GroupReduction:
     '''
@@ -120,6 +134,8 @@ class GroupReduction:
                 if 'zs' == v.VarName:
                     sol_zs = v.X
             print('Obj: %s' % m.ObjVal)
+            print('Cosine similarity: %s' % \
+                  cosine_similarity(wf, list(sol_wi.values())))
             print(*sol_wi.items(), sep='\n')
             print(*sol_wis.items(), sep='\n')
             print('z: %s' % sol_z)
@@ -138,8 +154,11 @@ class GroupReduction:
 
 
 if __name__ == "__main__":
-    input_groups = [[10.5, 20.1, 31.0, 39.7]]
+    #input_groups = [[10.5, 20.1, 31.0, 39.7]]
+    input_groups = [[1.0, 2.0, 3.0, 4.0]]
     group_reduction = GroupReduction(input_groups, 16*1024)
     output_groups = group_reduction.solve_sssg()
     print('Input: %s' % input_groups)
     print('Output: %s' % output_groups)
+    res = cosine_similarity(input_groups[0], output_groups[0])
+    print('Input/Output cosine similarity: %s' % res)
