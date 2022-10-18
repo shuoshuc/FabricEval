034c253d1cc9336d0ca3fe9897320f229e34dce7
Shawn Chen <shuoshuc@cs.cmu.edu>
2022-10-18 13:51:40 -0400

    Sweep both group and port.

--- wcmp_alloc/test_driver.py
+++ wcmp_alloc/test_driver.py
@@ -16,24 +16,25 @@ if __name__ == "__main__":
                          'fraction_digits'])
 
         for p in [2, 4, 8, 16]:
-            runtime = []
-            input_groups = input_groups_gen(g, p, lb, ub, frac_digits)
-            print('Input: %s' % input_groups)
-            group_reduction = GroupReduction(input_groups, table_limit)
+            for g in [2, 4, 8, 16]:
+                runtime = []
+                input_groups = input_groups_gen(g, p, lb, ub, frac_digits)
+                print('Input: %s' % input_groups)
+                group_reduction = GroupReduction(input_groups, table_limit)
 
-            # repeat 20 times for each run.
-            for run in range(20):
-                for solver in ['mip', 'heuristic']:
-                    start = time.time_ns()
-                    if solver == 'mip':
-                        output_groups = group_reduction.solve_sssg()
-                    else:
-                        output_groups = group_reduction.table_fitting_sssg()
-                    end = time.time_ns()
-                    print(f'Output [{solver}]: {output_groups}')
-                    l1_norm = l1_norm_diff(input_groups, output_groups)
-                    print(f'L1 Norm: {l1_norm}')
-                    solving_time = (end - start)/10**6
-                    print('Solving time (msec):', solving_time)
-                    writer.writerow([run, solver, solving_time, l1_norm, g, p,
-                                     lb, ub, frac_digits])
+                # repeat 20 times for each run.
+                for run in range(20):
+                    for solver in ['mip', 'heuristic']:
+                        start = time.time_ns()
+                        if solver == 'mip':
+                            output_groups = group_reduction.solve_sssg()
+                        else:
+                            output_groups = group_reduction.table_fitting_sssg()
+                        end = time.time_ns()
+                        print(f'Output [{solver}]: {output_groups}')
+                        l1_norm = l1_norm_diff(input_groups, output_groups)
+                        print(f'L1 Norm: {l1_norm}')
+                        solving_time = (end - start)/10**6
+                        print('Solving time (msec):', solving_time)
+                        writer.writerow([run, solver, solving_time, l1_norm, g,
+                                         p, lb, ub, frac_digits])
