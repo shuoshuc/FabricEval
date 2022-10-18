a12cec9ea6a568ca876b8c67589d087ad4cd643d
Shawn Chen <sschenshawn@gmail.com>
2022-10-18 13:58:00 -0400

    Enforce per-SSSG time limit.

--- wcmp_alloc/group_reduction.py
+++ wcmp_alloc/group_reduction.py
@@ -527,7 +527,7 @@ class GroupReduction:
                 #m.setParam("NodefileStart", 0.5)
                 m.setParam("NodefileDir", "/tmp")
                 m.setParam("Threads", 0)
-                #m.setParam("TimeLimit", 100)
+                m.setParam("TimeLimit", 120)
                 #m.setParam("LogFile", "gurobi.log")
 
                 # Construct model
