7e01a4e201f2a05b5a9123579794246bb90e9d43
Shawn Chen <shuoshuc@cs.cmu.edu>
2022-12-21 00:16:55 -0500

    Revert some changes.

--- e2e/run.py
+++ e2e/run.py
@@ -2,8 +2,6 @@ import csv
 import sys
 
 import numpy as np
-import proto.topology_pb2 as topo
-import proto.traffic_pb2 as traffic_pb2
 from google.protobuf import text_format
 
 from globalTE.global_te import GlobalTE
@@ -13,17 +11,15 @@ from topology.topology import Topology, filterPathSetWithSeg, loadTopo
 from traffic.tmgen import tmgen
 from traffic.traffic import Traffic
 
-TOY3_TE_SOL_PATH = 'tests/data/toy3_te_sol.textproto'
-
 if __name__ == "__main__":
     net_proto = generateToy3()
     toy3 = Topology('', net_proto)
     print('[Step 1] topology generated.')
     #print(text_format.MessageToString(net_proto))
-    traffic_proto = tmgen(tor_level=False,
+    traffic_proto = tmgen(tor_level=True,
                           cluster_vector=np.array([1]*22 + [2.5]*22 + [5]*21),
                           num_nodes=32,
-                          model='hot',
+                          model='gravity',
                           dist='exp')
     toy3_traffic = Traffic(toy3, '', traffic_proto)
     print('[Step 2] traffic demand generated.')
