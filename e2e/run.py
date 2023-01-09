e287bdbabe56c819bfbff1c17829a9403ce3e905
Shawn Chen <sschenshawn@gmail.com>
2023-01-09 02:15:01 -0500

    Change pipeline to dump files to a new folder. Flag to control whether to load TM and te_sol from a file.

--- e2e/run.py
+++ e2e/run.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 from datetime import datetime
+from pathlib import Path
 
 import numpy as np
 from google.protobuf import text_format
@@ -15,6 +16,8 @@ from traffic.traffic import Traffic
 
 TOY3_SOL = 'tests/data/te_sol.textproto'
 TOY3_TM = 'tests/data/toy3_traffic_gravity.textproto'
+# True to load TM and TE solution from the above files.
+LOAD_FILE = False
 
 if __name__ == "__main__":
     # Initializes global flags before running the pipeline.
@@ -33,30 +36,45 @@ if __name__ == "__main__":
     else:
         print(f'[ERROR] unknown group reduction algorithm {FLAG.GR_ALGO}.')
 
-    logpath = sys.argv[1]
+    logpath = Path(sys.argv[1] + f'/{FLAG.GR_ALGO}')
+    logpath.mkdir(parents=True, exist_ok=True)
+
+    # Generates topology.
     net_proto = generateToy3()
     toy3 = Topology('', net_proto)
     print(f'{datetime.now()} [Step 1] topology generated.', flush=True)
     #print(text_format.MessageToString(net_proto))
-    traffic_proto = tmgen(tor_level=True,
-                          cluster_vector=np.array([1]*22 + [2.5]*22 + [5]*21),
-                          num_nodes=32,
-                          model='gravity',
-                          dist='exp',
-                          netname='toy3')
-    with open(f'{logpath}/TM.textproto', 'w') as tm:
-        tm.write(text_format.MessageToString(traffic_proto))
-    toy3_traffic = Traffic(toy3, '', traffic_proto)
+
+    # Generates TM.
+    traffic_proto = None
+    if not LOAD_FILE:
+        traffic_proto = tmgen(tor_level=True,
+                              cluster_vector=np.array([1]*22 + [2.5]*22 + [5]*21),
+                              num_nodes=32,
+                              model='gravity',
+                              dist='exp',
+                              netname='toy3')
+        with (logpath / 'TM.textproto').open('w') as tm:
+            tm.write(text_format.MessageToString(traffic_proto))
+    toy3_traffic = Traffic(toy3, TOY3_TM, traffic_proto)
     print(f'{datetime.now()} [Step 2] traffic demand generated.', flush=True)
-    global_te = GlobalTE(toy3, toy3_traffic)
-    sol = global_te.solve()
-    with open(f'{logpath}/te_sol.textproto', 'w') as te_sol:
-        te_sol.write(text_format.MessageToString(sol))
+
+    # Runs global TE.
+    sol = None
+    if not LOAD_FILE:
+        global_te = GlobalTE(toy3, toy3_traffic)
+        sol = global_te.solve()
+        with (logpath / 'te_sol.textproto').open('w') as te_sol:
+            te_sol.write(text_format.MessageToString(sol))
     print(f'{datetime.now()} [Step 3] global TE solution generated.', flush=True)
     #print(text_format.MessageToString(sol))
-    wcmp_alloc = WCMPAllocation(toy3, toy3_traffic, '', sol)
+
+    # Runs local TE.
+    wcmp_alloc = WCMPAllocation(toy3, toy3_traffic, TOY3_SOL, sol)
     wcmp_alloc.run()
     print(f'{datetime.now()} [Step 4] local TE solution generated.', flush=True)
+
+    # Dumps stats.
     real_LUs = toy3.dumpRealLinkUtil()
     ideal_LUs = toy3.dumpIdealLinkUtil()
     delta_LUs = {}
@@ -64,7 +82,7 @@ if __name__ == "__main__":
         delta_LUs[k] = (u - ideal_LUs[k][0], dcn)
 
     print(f'{datetime.now()} [Step 5] dump link util to LU.csv', flush=True)
-    with open(f'{logpath}/LU.csv', 'w') as LU:
+    with (logpath / 'LU.csv').open('w') as LU:
         writer = csv.writer(LU)
         writer.writerow(["link name", "dcn facing", "ideal LU", "real LU", "delta"])
         for k, (v, dcn) in dict(sorted(delta_LUs.items(), key=lambda x: x[1][0],
@@ -75,7 +93,7 @@ if __name__ == "__main__":
     print(f'{datetime.now()} [Step 6] dump node table util to node_ecmp.csv',
           flush=True)
     ecmp_util = toy3.dumpECMPUtil()
-    with open(f'{logpath}/node_ecmp.csv', 'w') as ecmp:
+    with (logpath / 'node_ecmp.csv').open('w') as ecmp:
         writer = csv.writer(ecmp)
         writer.writerow(["node name", "ECMP util", "# groups"])
         for k, (util, num_g) in ecmp_util.items():
@@ -84,7 +102,7 @@ if __name__ == "__main__":
     print(f'{datetime.now()} [Step 7] dump node demand to node_demand.csv',
           flush=True)
     demand_admit = toy3.dumpDemandAdmission()
-    with open(f'{logpath}/node_demand.csv', 'w') as demand:
+    with (logpath / 'node_demand.csv').open('w') as demand:
         writer = csv.writer(demand)
         writer.writerow(["node name", "total demand", "total admit", "ratio"])
         for node, (tot_demand, tot_admit, f) in demand_admit.items():
