c685b3cba8a1eacb488c5471f8382c3ea550cb64
Shawn Chen <sschenshawn@gmail.com>
2023-05-10 01:29:00 -0400

    WIP: change striping solver to physical switch level.

--- topology/striping_plan.py
+++ topology/striping_plan.py
@@ -1,12 +1,30 @@
 import itertools
 import math
+import random
 
 import gurobipy as gp
+import numpy as np
 from gurobipy import GRB
 
 #import common.flags as FLAG
 #from common.common import PRINTV
 
+# Number of S3 switches per cluster.
+NUM_S3 = 4
+
+def matrixIndexToSwitchIndex(mi):
+    '''
+    Converts the index in the striping matrix to the corresponding cluster index
+    and S3 switch index.
+    '''
+    return mi // NUM_S3, mi % NUM_S3
+
+def switchIndexToMatrixIndex(ci, si):
+    '''
+    Converts the cluster index and S3 switch index to the corresponding index
+    in the striping matrix.
+    '''
+    return ci * NUM_S3 + si
 
 class StripingPlan:
     '''
@@ -25,24 +43,37 @@ class StripingPlan:
         '''
         self.num_clusters = num_clusters
         self.cluster_radices = cluster_radices
+        # A map from globally unique S3 index to its radix.
+        self.s3_radices = {}
+        # Distribute the cluster radix to each S3 switch. If the radix is
+        # not a multiple of NUM_S3, the residual links will be round robin
+        # assigned to the S3 switches of the cluster.
+        for ci in range(self.num_clusters):
+            for si in range(NUM_S3):
+                mi = switchIndexToMatrixIndex(ci, si)
+                self.s3_radices[mi] = self.cluster_radices[ci+1] // NUM_S3
+            for si in range(self.cluster_radices[ci+1] \
+                            - self.cluster_radices[ci+1] // NUM_S3 * NUM_S3):
+                mi = switchIndexToMatrixIndex(ci, si)
+                self.s3_radices[mi] += 1
 
     def _max_assignment(self, model):
         '''
         Builds an ILP formulation to maximize the number of links assigned
-        between clusters subject to radix and balance constraints.
+        between S3 switches subject to radix and balance constraints.
 
         model: pre-built empty model, needs decision vars and constraints.
         '''
-        min_radix = min(self.cluster_radices.values())
+        min_radix = min(self.s3_radices.values())
 
         # Create variables: x[i][j] is the number of links assigned between
-        # cluster i and j. x is a matrix of connectivity assignment.
+        # S3 switch i and j. x is a matrix of connectivity assignment.
         x = []
-        for i in range(self.num_clusters):
+        for i in range(self.num_clusters * NUM_S3):
             x_row = []
-            for j in range(self.num_clusters):
+            for j in range(self.num_clusters * NUM_S3):
                 x_row.append(model.addVar(vtype=GRB.INTEGER, lb=0,
-                                          ub=self.cluster_radices[i+1],
+                                          ub=self.s3_radices[i],
                                           name="x_{}_{}".format(i+1, j+1)))
             x.append(x_row)
         model.update()
@@ -51,22 +82,30 @@ class StripingPlan:
         model.setObjective(0.5 * gp.quicksum(model.getVars()), GRB.MAXIMIZE)
 
         # Add constraints.
-        for i in range(self.num_clusters):
-            # Add constraint: cluster radix bound.
-            model.addConstr(gp.quicksum(x[i]) <= self.cluster_radices[i+1],
-                            "cluster_radix_bound_{}".format(i+1))
-            for j in range(self.num_clusters):
+        for i in range(self.num_clusters * NUM_S3):
+            # Add constraint: S3 radix bound.
+            model.addConstr(gp.quicksum(x[i]) <= self.s3_radices[i],
+                            "s3_radix_bound_{}".format(i+1))
+            for j in range(self.num_clusters * NUM_S3):
                 if i == j:
                     # Add constraint: no self loops.
                     model.addConstr(x[i][i] == 0, "no_self_loop_{}".format(i+1))
                     continue
+                _, si = matrixIndexToSwitchIndex(i)
+                _, sj = matrixIndexToSwitchIndex(j)
+                if si != sj:
+                    # Add constraint: no cross connect between S3 switches of
+                    # different locations (intra-cluster indices).
+                    model.addConstr(x[i][j] == 0,
+                                    "no_x_connect_{}_{}".format(i+1, j+1))
+                    continue
                 # Add constraint: equal spread of links to peers.
-                # Note: lower bound should be the cluster of min radix, since
+                # Note: lower bound should be the S3 switch of min radix, since
                 # we allow ports to be idle.
-                model.addConstr(x[i][j] >= math.floor(min_radix
+                model.addConstr(x[i][j] >= math.floor(min_radix \
                                                      / (self.num_clusters - 1)),
                                 "even_spread_lb_{}_{}".format(i+1, j+1))
-                model.addConstr(x[i][j] <= math.ceil(self.cluster_radices[i+1]\
+                model.addConstr(x[i][j] <= math.ceil(self.s3_radices[i] \
                                                      / (self.num_clusters - 1)),
                                 "even_spread_ub_{}_{}".format(i+1, j+1))
                 # Matrix x is symmetric, we can save half of the constraints.
@@ -80,7 +119,7 @@ class StripingPlan:
     def solve(self):
         try:
             # Initialize a new model
-            m = gp.Model("cluster_striping")
+            m = gp.Model("striping")
             #m.setParam("LogToConsole", 1 if FLAG.VERBOSE >= 2 else 0)
             m.setParam("LogToConsole", 1)
             m.setParam("FeasibilityTol", 1e-7)
@@ -100,15 +139,30 @@ class StripingPlan:
             # Optimize model
             m.optimize()
 
-            sol_x = dict()
-            for v in m.getVars():
-                split = v.VarName.split('_')
-                i, j = int(split[1]) - 1, int(split[2]) - 1
-                sol_x[v.VarName] = v.X
             #PRINTV(2, 'Obj: %s' % m.ObjVal)
-            #PRINTV(2, str(sol_x))
-            print('Obj: %s' % m.ObjVal)
-            print(str(sol_x))
+            print(f'[Obj] max links assigned in DCN: {m.ObjVal}')
+
+            # A striping matrix for all S3 switches.
+            mat_s3 = np.zeros(shape=(self.num_clusters * NUM_S3,
+                                     self.num_clusters * NUM_S3))
+            for v in m.getVars():
+                tot_links = int(v.X)
+                if not tot_links:
+                    continue
+                split = v.VarName.split('_')
+                # Extract the two S3 switch indices.
+                i, j = int(split[1]) - 1, int(split[2]) - 1
+                mat_s3[i][j] = tot_links
+                '''
+                # The striping matrix is symmetric, we only need to process half
+                # of it.
+                if i >= j:
+                    continue
+                ci, si = matrixIndexToSwitchIndex(i)
+                cj, sj = matrixIndexToSwitchIndex(j)
+                print(f'c{ci+1}-s3i{si+1} -> c{cj+1}-s3i{sj+1}: {tot_links} links.')
+                '''
+            print(mat_s3)
 
             return None
 
@@ -120,6 +174,7 @@ class StripingPlan:
             return []
 
 if __name__ == "__main__":
+    '''
     cluster_radices = {
         1: 16,
         2: 16,
@@ -127,4 +182,11 @@ if __name__ == "__main__":
         4: 16
     }
     sp = StripingPlan(4, cluster_radices)
+    '''
+    cluster_radices = {
+        1: 8,
+        2: 16,
+        3: 16,
+    }
+    sp = StripingPlan(3, cluster_radices)
     sp.solve()
