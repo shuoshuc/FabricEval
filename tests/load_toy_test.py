b9ef90b133f12f676ef128724b36c5045701fc9c
Shawn Chen <shuoshuc@cs.cmu.edu>
2023-12-11 18:11:08 -0500

    Get off git lfs and clean up toy fabrics.

--- tests/load_toy_test.py
+++ tests/load_toy_test.py
@@ -11,16 +11,6 @@ from topology.topology import Topology, filterPathSetWithSeg, loadTopo
 from traffic.tmgen import tmgen
 from traffic.traffic import Traffic, loadTraffic
 
-P1 = 'toy1-c1-ab1-s2i1-p1'
-P2 = 'toy1-c1-ab1-s2i1-p2'
-P3 = 'toy1-c1-ab1-s2i2-p1'
-P4 = 'toy1-c1-ab1-s2i2-p2'
-P5 = 'toy1-c1-ab1-s3i1-p1'
-P6 = 'toy1-c1-ab1-s3i1-p2'
-P7 = 'toy1-c1-ab1-s3i2-p1'
-P8 = 'toy1-c1-ab1-s3i2-p2'
-# Links in toy1: P1-P5, P2-P7, P3-P6, P4-P8.
-TOY1_PATH = 'tests/data/toy1.textproto'
 P9 = 'toy2-c3-ab1-s1i1-p1'
 P10 = 'toy2-c3-ab1-s2i1-p1'
 P11 = 'toy2-c1-ab1-s3i2-p4'
@@ -107,6 +97,7 @@ F2_AGGR_BLOCK1 = 'f2-c1-ab1'
 F2_AGGR_BLOCK2 = 'f2-c2-ab1'
 F2_TOR1 = 'f2-c1-ab1-s1i1'
 
+
 class TestLoadToyNet(unittest.TestCase):
     def test_load_invalid_topo(self):
         self.assertEqual(None, loadTopo(''))
@@ -115,48 +106,8 @@ class TestLoadToyNet(unittest.TestCase):
         self.assertEqual(None, loadTraffic(''))
 
     def test_load_valid_toynet(self):
-        toy1 = loadTopo(TOY1_PATH)
+        toy1 = loadTopo(TOY2_PATH)
         self.assertNotEqual(None, toy1)
-
-    def test_toy1_entities(self):
-        toy1 = loadTopo(TOY1_PATH)
-        # check network
-        self.assertEqual('toy1', toy1.name)
-        self.assertEqual(1, len(toy1.clusters))
-        self.assertEqual(0, len(toy1.paths))
-        self.assertEqual(8, len(toy1.links))
-        # check cluster
-        cluster = toy1.clusters[0]
-        self.assertEqual('toy1-c1', cluster.name)
-        self.assertEqual(1, len(cluster.aggr_blocks))
-        # check aggr block
-        aggr_block = cluster.aggr_blocks[0]
-        self.assertEqual('toy1-c1-ab1', aggr_block.name)
-        self.assertEqual(4, len(aggr_block.nodes))
-        # check all nodes
-        for node in aggr_block.nodes:
-            self.assertEqual(2, len(node.ports))
-            self.assertEqual(4000, node.flow_limit)
-            self.assertEqual(4000, node.ecmp_limit)
-            self.assertEqual(128, node.group_limit)
-        # check node s2i1
-        s2i1 = aggr_block.nodes[0]
-        self.assertEqual('toy1-c1-ab1-s2i1', s2i1.name)
-        self.assertEqual(2, s2i1.stage)
-        self.assertEqual(1, s2i1.index)
-        # check all ports
-        for port in s2i1.ports:
-            self.assertEqual(100 * 1000, port.port_speed_mbps)
-            self.assertEqual(False, port.dcn_facing)
-        # check port s2i1-p1
-        port1 = s2i1.ports[0]
-        self.assertEqual('toy1-c1-ab1-s2i1-p1', port1.name)
-        # check 1st link
-        link = toy1.links[0]
-        self.assertEqual(P1 + ':' + P5, link.name)
-        self.assertEqual(P1, link.src_port_id)
-        self.assertEqual(P5, link.dst_port_id)
-        self.assertEqual(100 * 1000, link.link_speed_mbps)
 
     def test_toy2_topology_construction(self):
         toy2 = Topology(TOY2_PATH)
@@ -216,9 +167,11 @@ class TestLoadToyNet(unittest.TestCase):
         toy2 = Topology(TOY2_PATH)
         toy2_traffic = Traffic(toy2, TOY2_TRAFFIC_PATH)
         self.assertEqual(2, len(toy2_traffic.getAllDemands()))
-        self.assertEqual({('toy2-c1-ab1', 'toy2-c3-ab1'): 300000,
-                          ('toy2-c3-ab1', 'toy2-c1-ab1'): 100000},
-                         toy2_traffic.getAllDemands())
+        self.assertEqual(
+            {
+                ('toy2-c1-ab1', 'toy2-c3-ab1'): 300000,
+                ('toy2-c3-ab1', 'toy2-c1-ab1'): 100000
+            }, toy2_traffic.getAllDemands())
 
     def test_toy2_topology_serialization(self):
         toy2 = Topology(TOY2_PATH)
@@ -227,6 +180,7 @@ class TestLoadToyNet(unittest.TestCase):
         self.assertEqual('toy2', toy2_proto.name)
         self.assertEqual(3, len(toy2_proto.clusters))
         self.assertEqual(6, len(toy2_proto.paths))
+
 
 class TestLoadToy3Net(unittest.TestCase):
     def test_toy3_topology_construction(self):
@@ -237,8 +191,9 @@ class TestLoadToy3Net(unittest.TestCase):
         self.assertEqual(65 * 40, toy3.numNodes())
         # 8 * 32 * 2 * 65 S1-S2 links per cluster, 64 * 4 * 2 * 65 S2-S3 links,
         # 64 * 4 * 65 S3-S3 links.
-        self.assertEqual(8*32*2*65+64*4*2*65+64*4*65, toy3.numLinks())
-        self.assertEqual(65*64, len(toy3.getAllPaths()))
+        self.assertEqual(8 * 32 * 2 * 65 + 64 * 4 * 2 * 65 + 64 * 4 * 65,
+                         toy3.numLinks())
+        self.assertEqual(65 * 64, len(toy3.getAllPaths()))
         # Path between two 40G clusters: 4 * 40
         self.assertEqual(160000, toy3.findCapacityOfPath(TOY3_PATH1))
         # Path between a 40G cluster and a 200G cluster: 4 * 40
@@ -291,15 +246,14 @@ class TestLoadToy3Net(unittest.TestCase):
         self.assertEqual(TOY3_AGGR_BLOCK2,
                          toy3.findAggrBlockOfToR(TOY3_TOR2).name)
         # Verify the stage and index of ToR1.
-        self.assertEqual(1,
-                         toy3.getNodeByName(TOY3_TOR1).stage)
-        self.assertEqual(1,
-                         toy3.getNodeByName(TOY3_TOR1).index)
+        self.assertEqual(1, toy3.getNodeByName(TOY3_TOR1).stage)
+        self.assertEqual(1, toy3.getNodeByName(TOY3_TOR1).index)
 
     def test_toy3_traffic_construction1(self):
         toy3 = Topology('', input_proto=generateToy3())
         traffic_proto = tmgen(tor_level=False,
-                              cluster_vector=np.array([1]*22+[2.5]*22+[5]*21),
+                              cluster_vector=np.array([1] * 22 + [2.5] * 22 +
+                                                      [5] * 21),
                               num_nodes=32,
                               model='flat',
                               dist='',
@@ -309,16 +263,17 @@ class TestLoadToy3Net(unittest.TestCase):
                          toy3_traffic.getDemandType())
         self.assertEqual(64 * 65, len(toy3_traffic.getAllDemands()))
         # Flat demand matrix has the same volume in both directions.
-        self.assertEqual(80000, toy3_traffic.getDemand(TOY3_AGGR_BLOCK1,
-                                                       TOY3_AGGR_BLOCK2))
-        self.assertEqual(80000, toy3_traffic.getDemand(TOY3_AGGR_BLOCK2,
-                                                       TOY3_AGGR_BLOCK1))
+        self.assertEqual(
+            80000, toy3_traffic.getDemand(TOY3_AGGR_BLOCK1, TOY3_AGGR_BLOCK2))
+        self.assertEqual(
+            80000, toy3_traffic.getDemand(TOY3_AGGR_BLOCK2, TOY3_AGGR_BLOCK1))
 
     def test_toy3_traffic_construction2(self):
         FLAG.P_SPARSE = 0.1
         toy3 = Topology('', input_proto=generateToy3())
         traffic_proto = tmgen(tor_level=False,
-                              cluster_vector=np.array([1]*22+[2.5]*22+[5]*21),
+                              cluster_vector=np.array([1] * 22 + [2.5] * 22 +
+                                                      [5] * 21),
                               num_nodes=32,
                               model='gravity',
                               dist='exp',
@@ -337,7 +292,8 @@ class TestLoadToy3Net(unittest.TestCase):
         FLAG.P_SPARSE = 0.0
         toy3 = Topology('', input_proto=generateToy3())
         traffic_proto = tmgen(tor_level=False,
-                              cluster_vector=np.array([1]*22+[2.5]*22+[5]*21),
+                              cluster_vector=np.array([1] * 22 + [2.5] * 22 +
+                                                      [5] * 21),
                               num_nodes=32,
                               model='gravity',
                               dist='exp',
@@ -362,12 +318,14 @@ class TestLoadToy3Net(unittest.TestCase):
             dst_block_name = f'toy3-c{i}-ab1'
             tot_traffic += toy3_traffic.getDemand(TOY3_AGGR_BLOCK1,
                                                   dst_block_name)
-        self.assertTrue(tot_traffic < 40000*22*4 + 100000*22*4 + 200000*20*4)
+        self.assertTrue(
+            tot_traffic < 40000 * 22 * 4 + 100000 * 22 * 4 + 200000 * 20 * 4)
 
     def test_toy3_traffic_construction4(self):
         toy3 = Topology('', input_proto=generateToy3())
         traffic_proto = tmgen(tor_level=True,
-                              cluster_vector=np.array([1]*22+[2.5]*22+[5]*21),
+                              cluster_vector=np.array([1] * 22 + [2.5] * 22 +
+                                                      [5] * 21),
                               num_nodes=32,
                               model='flat',
                               dist='',
@@ -379,10 +337,13 @@ class TestLoadToy3Net(unittest.TestCase):
         # Flat demand matrix has the same volume in both directions.
         # The inter-block demand should be 153 Mbps (per-ToR) * 32 peer ToRs *
         # 32 sister ToRs.
-        self.assertEqual(115*32*32, toy3_traffic.getDemand(TOY3_AGGR_BLOCK1,
-                                                           TOY3_AGGR_BLOCK2))
-        self.assertEqual(115*32*32, toy3_traffic.getDemand(TOY3_AGGR_BLOCK2,
-                                                           TOY3_AGGR_BLOCK1))
+        self.assertEqual(
+            115 * 32 * 32,
+            toy3_traffic.getDemand(TOY3_AGGR_BLOCK1, TOY3_AGGR_BLOCK2))
+        self.assertEqual(
+            115 * 32 * 32,
+            toy3_traffic.getDemand(TOY3_AGGR_BLOCK2, TOY3_AGGR_BLOCK1))
+
 
 class TestLoadToy4Net(unittest.TestCase):
     def test_toy4_topology_construction(self):
@@ -390,7 +351,7 @@ class TestLoadToy4Net(unittest.TestCase):
         self.assertEqual(5, toy4.numClusters())
         # 8 + 32 nodes per cluster
         self.assertEqual(5 * 12, toy4.numNodes())
-        self.assertEqual(5*4, len(toy4.getAllPaths()))
+        self.assertEqual(5 * 4, len(toy4.getAllPaths()))
         # All paths in Toy4 have 160G capacity.
         for path in toy4.getAllPaths().values():
             self.assertEqual(160000, path.capacity)
@@ -438,7 +399,7 @@ class TestLoadToy4Net(unittest.TestCase):
     def test_toy4_traffic_construction(self):
         toy4 = Topology('', input_proto=generateToy4())
         traffic_proto = tmgen(tor_level=False,
-                              cluster_vector=np.array([1]*5),
+                              cluster_vector=np.array([1] * 5),
                               num_nodes=4,
                               model='single',
                               dist='',
@@ -450,9 +411,10 @@ class TestLoadToy4Net(unittest.TestCase):
         self.assertEqual(1, len(toy4_traffic.getAllDemands()))
         self.assertTrue((TOY4_AGGR_BLOCK1, TOY4_AGGR_BLOCK2) in \
                         toy4_traffic.getAllDemands())
-        self.assertEqual(40000 * 4 * 0.5,
-                         toy4_traffic.getAllDemands()[(TOY4_AGGR_BLOCK1,
-                                                       TOY4_AGGR_BLOCK2)])
+        self.assertEqual(
+            40000 * 4 * 0.5,
+            toy4_traffic.getAllDemands()[(TOY4_AGGR_BLOCK1, TOY4_AGGR_BLOCK2)])
+
 
 class TestLoadToy5Net(unittest.TestCase):
     def test_toy5_topology_construction(self):
@@ -511,7 +473,8 @@ class TestLoadToy5Net(unittest.TestCase):
     def test_toy5_traffic_construction1(self):
         toy5 = Topology('', input_proto=generateToy5())
         traffic_proto = tmgen(tor_level=False,
-                              cluster_vector=np.array([1]*11+[2.5]*11+[5]*11),
+                              cluster_vector=np.array([1] * 11 + [2.5] * 11 +
+                                                      [5] * 11),
                               num_nodes=16,
                               model='flat',
                               dist='',
@@ -522,15 +485,16 @@ class TestLoadToy5Net(unittest.TestCase):
         self.assertEqual(33 * 32, len(toy5_traffic.getAllDemands()))
         # Flat demand matrix has the same volume in both directions.
         # The inter-block demand should be 160000 Mbps when tor_level=False.
-        self.assertEqual(160000, toy5_traffic.getDemand(TOY5_AGGR_BLOCK1,
-                                                        TOY5_AGGR_BLOCK2))
-        self.assertEqual(160000, toy5_traffic.getDemand(TOY5_AGGR_BLOCK2,
-                                                        TOY5_AGGR_BLOCK1))
+        self.assertEqual(
+            160000, toy5_traffic.getDemand(TOY5_AGGR_BLOCK1, TOY5_AGGR_BLOCK2))
+        self.assertEqual(
+            160000, toy5_traffic.getDemand(TOY5_AGGR_BLOCK2, TOY5_AGGR_BLOCK1))
 
     def test_toy5_traffic_construction2(self):
         toy5 = Topology('', input_proto=generateToy5())
         traffic_proto = tmgen(tor_level=False,
-                              cluster_vector=np.array([1]*11+[2.5]*11+[5]*11),
+                              cluster_vector=np.array([1] * 11 + [2.5] * 11 +
+                                                      [5] * 11),
                               num_nodes=16,
                               model='gravity',
                               dist='exp',
@@ -540,14 +504,15 @@ class TestLoadToy5Net(unittest.TestCase):
                          toy5_traffic.getDemandType())
         self.assertEqual(33 * 32, len(toy5_traffic.getAllDemands()))
         # Gravity demand matrix should not set the 40G blocks to empty.
-        self.assertNotEqual(0, toy5_traffic.getDemand(TOY5_AGGR_BLOCK1,
-                                                      TOY5_AGGR_BLOCK2))
-        self.assertNotEqual(0, toy5_traffic.getDemand(TOY5_AGGR_BLOCK2,
-                                                      TOY5_AGGR_BLOCK1))
+        self.assertNotEqual(
+            0, toy5_traffic.getDemand(TOY5_AGGR_BLOCK1, TOY5_AGGR_BLOCK2))
+        self.assertNotEqual(
+            0, toy5_traffic.getDemand(TOY5_AGGR_BLOCK2, TOY5_AGGR_BLOCK1))
+
 
 class TestLoadF1Net(unittest.TestCase):
     def test_f1_topology_construction(self):
-        f1= Topology('', input_proto=generateF1())
+        f1 = Topology('', input_proto=generateF1())
         self.assertEqual(33, f1.numClusters())
         # 8 + 16 nodes per cluster
         self.assertEqual(33 * 24, f1.numNodes())
@@ -561,8 +526,7 @@ class TestLoadF1Net(unittest.TestCase):
         links = [l.name for l in f1.findLinksOfPath(F1_PATH1)]
         self.assertTrue(F1_LINK1 in links)
         # Verify S3-S3 port and peer.
-        self.assertEqual(F1_PEER_PORT1,
-                         f1.findPeerPortOfPort(F1_PORT1).name)
+        self.assertEqual(F1_PEER_PORT1, f1.findPeerPortOfPort(F1_PORT1).name)
         # Verify that DCN ports have odd port indices.
         p1 = f1.getPortByName(F1_PORT1)
         self.assertEqual(F1_PORT1, p1.name)
@@ -572,8 +536,7 @@ class TestLoadF1Net(unittest.TestCase):
         self.assertTrue(pp1.dcn_facing)
         self.assertEqual(1, pp1.index % 2)
         # Verify S2-S3 port and peer.
-        self.assertEqual(F1_PEER_PORT2,
-                         f1.findPeerPortOfPort(F1_PORT2).name)
+        self.assertEqual(F1_PEER_PORT2, f1.findPeerPortOfPort(F1_PORT2).name)
         # Verify that S2-facing S3 ports have even indices.
         p2 = f1.getPortByName(F1_PORT2)
         self.assertFalse(p2.dcn_facing)
@@ -583,8 +546,7 @@ class TestLoadF1Net(unittest.TestCase):
         self.assertFalse(pp2.dcn_facing)
         self.assertEqual(1, pp2.index % 2)
         # Verify S1-S2 port and peer.
-        self.assertEqual(F1_PEER_PORT3,
-                         f1.findPeerPortOfPort(F1_PORT3).name)
+        self.assertEqual(F1_PEER_PORT3, f1.findPeerPortOfPort(F1_PORT3).name)
         # Verify that S1-facing S2 ports have even indices.
         p3 = f1.getPortByName(F1_PORT3)
         self.assertFalse(p3.dcn_facing)
@@ -594,14 +556,14 @@ class TestLoadF1Net(unittest.TestCase):
         self.assertFalse(pp3.dcn_facing)
         self.assertEqual(1, pp3.index % 2)
         # Verify the 'virutal' parent of ToRs.
-        self.assertEqual(F1_AGGR_BLOCK1,
-                         f1.findAggrBlockOfToR(F1_TOR1).name)
+        self.assertEqual(F1_AGGR_BLOCK1, f1.findAggrBlockOfToR(F1_TOR1).name)
         # Verify the stage and index of ToR1.
         self.assertEqual(1, f1.getNodeByName(F1_TOR1).stage)
 
+
 class TestLoadF2Net(unittest.TestCase):
     def test_f2_topology_construction(self):
-        f2= Topology('', input_proto=generateF2())
+        f2 = Topology('', input_proto=generateF2())
         self.assertEqual(5, f2.numClusters())
         # 8 + 16 nodes per cluster
         self.assertEqual(5 * 24, f2.numNodes())
@@ -615,8 +577,7 @@ class TestLoadF2Net(unittest.TestCase):
         links = [l.name for l in f2.findLinksOfPath(F2_PATH1)]
         self.assertTrue(F2_LINK1 in links)
         # Verify S3-S3 port and peer.
-        self.assertEqual(F2_PEER_PORT1,
-                         f2.findPeerPortOfPort(F2_PORT1).name)
+        self.assertEqual(F2_PEER_PORT1, f2.findPeerPortOfPort(F2_PORT1).name)
         # Verify that DCN ports have odd port indices.
         p1 = f2.getPortByName(F2_PORT1)
         self.assertEqual(F2_PORT1, p1.name)
@@ -626,8 +587,7 @@ class TestLoadF2Net(unittest.TestCase):
         self.assertTrue(pp1.dcn_facing)
         self.assertEqual(1, pp1.index % 2)
         # Verify S2-S3 port and peer.
-        self.assertEqual(F2_PEER_PORT2,
-                         f2.findPeerPortOfPort(F2_PORT2).name)
+        self.assertEqual(F2_PEER_PORT2, f2.findPeerPortOfPort(F2_PORT2).name)
         # Verify that S2-facing S3 ports have even indices.
         p2 = f2.getPortByName(F2_PORT2)
         self.assertFalse(p2.dcn_facing)
@@ -637,8 +597,7 @@ class TestLoadF2Net(unittest.TestCase):
         self.assertFalse(pp2.dcn_facing)
         self.assertEqual(1, pp2.index % 2)
         # Verify S1-S2 port and peer.
-        self.assertEqual(F2_PEER_PORT3,
-                         f2.findPeerPortOfPort(F2_PORT3).name)
+        self.assertEqual(F2_PEER_PORT3, f2.findPeerPortOfPort(F2_PORT3).name)
         # Verify that S1-facing S2 ports have even indices.
         p3 = f2.getPortByName(F2_PORT3)
         self.assertFalse(p3.dcn_facing)
@@ -648,10 +607,10 @@ class TestLoadF2Net(unittest.TestCase):
         self.assertFalse(pp3.dcn_facing)
         self.assertEqual(1, pp3.index % 2)
         # Verify the 'virutal' parent of ToRs.
-        self.assertEqual(F2_AGGR_BLOCK1,
-                         f2.findAggrBlockOfToR(F2_TOR1).name)
+        self.assertEqual(F2_AGGR_BLOCK1, f2.findAggrBlockOfToR(F2_TOR1).name)
         # Verify the stage and index of ToR1.
         self.assertEqual(1, f2.getNodeByName(F2_TOR1).stage)
+
 
 if __name__ == "__main__":
     unittest.main()
