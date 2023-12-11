e518fcea4aa4d1d9ddcba718160f385145e3ddfb
Shawn Chen <sschenshawn@gmail.com>
2023-12-11 14:13:40 -0500

    Check git lfs and Gurobi version/license in test_dep.py

--- scripts/test_dep.py
+++ scripts/test_dep.py
@@ -6,6 +6,7 @@
 import subprocess
 import sys
 
+import gurobipy as gp
 import numpy as np
 import scipy as sp
 
@@ -23,9 +24,20 @@ def test_dep():
     if str2tup(sp.__version__) < (1, 8, 0):
         print("ERROR: scipy version below 1.8.0")
         return
+    if gp.gurobi.version() < (9, 5, 0):
+        print("ERROR: Gurobi version below 9.5.0")
+        return
+    gurobi_license = str(subprocess.run(['gurobi_cl', '--license'], capture_output=True).stdout)
+    if 'expired' in gurobi_license:
+        print("ERROR: Gurobi license expired")
+        return
     git_ver = str(subprocess.run(['git', '--version'], capture_output=True).stdout)
     if 'git version' not in git_ver:
         print("ERROR: could not find git")
+        return
+    gitlfs_ver = str(subprocess.run(['git', 'lfs', '--version'], capture_output=True).stdout)
+    if 'git-lfs' not in gitlfs_ver:
+        print("ERROR: could not find git lfs")
         return
     bazel_ver = str(subprocess.run(['bazel', '--version'], capture_output=True).stdout)
     if 'bazel' not in bazel_ver:
